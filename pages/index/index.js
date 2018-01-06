118c75411f09137ada8b5a0ad50a81b692f6d827
dyh-sjtu <dyh_sjtu@163.com>
2018-01-07 01:15:13 +0800

    增加多项搜索

--- pages/index/index.js
+++ pages/index/index.js
@@ -55,14 +55,19 @@ const formatDay = require('../../utils/util');
 //   }
 // })
 Page({
-	data:{
-		categories: [],
-		categoriesArr: [],
-		cat_index: null,
-		multiCondition: [['不限', '1-3天', '3天以上'], ['不限', '1-3月', '3月以上'], ['不限', '100-200', '200以上'], ['不限', '大专', '本科', '硕士及以上'], ['不限', '不可转正', '可转正']],
-		multiIndex:[0,0,0,0,0],
-		multiIndexLen: 0,
-		jobs:[]
+	data: {
+		autofocus: false,
+		focus: false,  // 聚焦后拉起软键盘
+		queryData: {}, // 筛选条件
+		categories: [], // 岗位类型数组
+		categoriesArr: [], // 总共多少岗位种类
+		categoryId: null, // 此时选定的查询岗位类别
+		cat_index: null, // 此时选定的岗位类别顺序
+		multiCondition: [['不限', '1-3天', '3天以上'], ['不限', '1-3月', '3月以上'], ['不限', '0-250', '250以上'], ['不限', '专科', '本科', '硕士及以上'], ['不限', '不可转正', '可转正']],
+		multiIndex: [0, 0, 0, 0, 0], // 多列选择选定的筛选条件
+		multiIndexLen: 0, // 多列选择筛选条件的长度
+		jobs: [],
+		jobsTmp:[]
 	},
 	
 	onLoad: function () {
@@ -87,7 +92,6 @@ Page({
 						categories: data.data,
 						categoriesArr: categoriesArr
 					});
-					console.log(that.data.categories)
 				}
 			}
 		});
@@ -102,14 +106,15 @@ Page({
 						item.meta.updateAt = formatDay.formatDay(item.meta.updateAt);
 					});
 					that.setData({
-						jobs: data.data
+						jobs: data.data,
+						jobsTmp: data.data
 					})
 				}
-				console.log(that.data.jobs)
 			}
 		})
 	},
-	bindPickerChangeCategories: function(e) {
+	// 点击修改岗位类型搜索
+	bindPickerChangeCategories: function (e) {
 		wxRequest('category', {
 			method: 'get',
 			data: {
@@ -118,22 +123,85 @@ Page({
 			success: (res) => {
 				let data = res.data;
 				if (data.success) {
+					data.data.forEach((item) => {
+						item.meta.updateAt = formatDay.formatDay(item.meta.updateAt);
+					});
+					
 					this.setData({
-						jobs: data.data
+						jobs: data.data,
+						jobsTmp: data.data
 					})
 				}
 			}
 		});
-		console.log(this.data.categories[e.detail.value]._id)
 		
-	  this.setData({
-		  cat_index: e.detail.value
-	  })
-  },
-  
+		this.setData({
+			cat_index: e.detail.value,
+			categoryId: this.data.categories[e.detail.value]._id
+		})
+	},
+	
 	// 点击修改筛选条件
 	bindMultiPickerChange: function (e) {
-		console.log('picker发送选择改变，携带值为', e.detail.value);
+		
+		// 本地查询，todo 考虑后续改善数据结构在服务器上查询
+		let jobs = this.data.jobsTmp;
+		let queryData = {};
+		if (this.data.categoryId) {
+			queryData.categoryId = this.data.categoryId;
+			jobs = jobs.filter((item) => {
+				return item.category.toString() == queryData.categoryId.toString();
+			})
+		}
+		if (e.detail.value[0] != 0) {
+			queryData.internWeek = this.data.multiCondition[0][e.detail.value[0]];
+			if (e.detail.value[0] == 1) {
+				jobs = jobs.filter((item) => {
+					return parseInt(item.internWeek) < Number(parseInt(queryData.internWeek.split('-')[1])) && parseInt(item.internWeek) >= Number(queryData.internWeek.split('-')[0]);
+				})
+			} else {
+				jobs = jobs.filter((item) => {
+					return parseInt(item.internWeek) >= parseInt(queryData.internWeek);
+				})
+			}
+		}
+		if (e.detail.value[1] != 0) {
+			queryData.internMonth = this.data.multiCondition[1][e.detail.value[1]];
+			if (e.detail.value[1] == 1) {
+				jobs = jobs.filter((item) => {
+					return parseInt(item.internMonth) < Number(parseInt(queryData.internMonth.split('-')[1])) && parseInt(item.internMonth) >= Number(queryData.internMonth.split('-')[0]);
+				})
+			} else {
+				jobs = jobs.filter((item) => {
+					return parseInt(item.internMonth) >= parseInt(queryData.internMonth);
+				})
+			}
+		}
+		if (e.detail.value[2] != 0) {
+			queryData.salary = this.data.multiCondition[2][e.detail.value[2]];
+			if (e.detail.value[2] == 1) {
+				jobs = jobs.filter((item) => {
+					return parseInt(item.salary) <= Number(queryData.salary.split('-')[1]) && parseInt(item.salary) >= Number(queryData.salary.split('-')[0]);
+				})
+			} else {
+				jobs = jobs.filter((item) => {
+					return parseInt(item.salary) > parseInt(queryData.salary);
+				})
+			}
+		}
+		if (e.detail.value[3] != 0) {
+			queryData.education = this.data.multiCondition[3][e.detail.value[3]];
+			jobs = jobs.filter((item) => {
+				return item.education == queryData.education;
+			})
+		}
+		if (e.detail.value[4] != 0) {
+			queryData.canBeRegular = this.data.multiCondition[4][e.detail.value[4]];
+			jobs = jobs.filter((item) => {
+				return item.canBeRegular == '不详' || item.canBeRegular == queryData.canBeRegular;
+			})
+		}
+		
 		let len = 0;
 		e.detail.value.forEach((item) => {
 			item !== 0 ? len++ : len;
@@ -141,18 +209,34 @@ Page({
 		
 		this.setData({
 			multiIndex: e.detail.value,
-			multiIndexLen: len
-		})
+			multiIndexLen: len,
+			queryData: queryData,
+			jobs: jobs
+		});
+		
 	},
-
+	
 	
 	// 点击进入详情页面
-  detail: function(e) {
+	detail: function (e) {
 		console.log(e);
-    wx.navigateTo({
-	    url: `../detail/detail?job=${e.currentTarget.dataset['job']}`
-    })
-  }
+		wx.navigateTo({
+			url: `../detail/detail?job=${e.currentTarget.dataset['job']}`
+		})
+	},
+	
+	//  聚焦，获取软键盘
+	getKeyboard: function () {
+		this.setData({
+			focus: true
+		})
+	},
+	
+	bindButtonTap: function () {
+		this.setData({
+			autofocus: true
+		})
+	}
 });
 
 
